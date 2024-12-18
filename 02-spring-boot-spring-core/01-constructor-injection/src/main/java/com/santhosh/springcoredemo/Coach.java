ef95a2db7166d0068f7ad13105c6f3906e7fbe21
santhosh-repo <santhoshcharugundla.dev@gmail.com>
2024-12-18 12:18:09 +0530

    Spring Core
    Spring Core

---
+++ 02-spring-boot-spring-core/01-constructor-injection/src/main/java/com/santhosh/springcoredemo/Coach.java
@@ -0,0 +1,6 @@
+package com.santhosh.springcoredemo;
+
+public interface Coach {
+
+    String getDailyWorkout();
+}
---
+++ 02-spring-boot-spring-core/02-component-scanning/src/main/java/com/santhosh/springcoredemo/rest/DemoController.java
@@ -0,0 +1,23 @@
+package com.santhosh.springcoredemo.rest;
+
+import com.santhosh.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    private Coach myCoach;
+
+    @Autowired
+    public DemoController(Coach myCoach) {
+        this.myCoach = myCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout(){
+        return myCoach.getDailyWorkout();
+    }
+
+}
