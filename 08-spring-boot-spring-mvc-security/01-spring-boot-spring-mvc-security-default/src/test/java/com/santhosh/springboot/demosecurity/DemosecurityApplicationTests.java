bea8a7befb8fd648bffbeb83c92704a56e161e2b
santhosh-repo <santhoshcharugundla.dev@gmail.com>
2024-12-18 12:41:24 +0530

    Spring-MVC-Security

---
+++ 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/test/java/com/santhosh/springboot/demosecurity/DemosecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santhosh.springboot.demosecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemosecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
